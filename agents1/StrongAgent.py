d8faa62c17a41b7fc63132532d80f9d3eeab0495
Zohar Cochavi <cochavi.zohar@protonmail.com>
2022-03-20 11:37:49 +0100

    Can now find collectable objects in room

--- agents1/StrongAgent.py
+++ agents1/StrongAgent.py
@@ -1,4 +1,8 @@
-from agents1.CustomBaselineAgent import CustomBaselineAgent, Phase
+from agents1.CustomBaselineAgent import Action, CustomBaselineAgent, Phase
+
+from matrx.grid_world import GridWorld
+
+from bw4t.BW4TBlocks import GhostBlock
 
 class StrongAgent(CustomBaselineAgent):
     """
@@ -11,18 +15,22 @@ class StrongAgent(CustomBaselineAgent):
 
     # ==== PHASE FUNCTIONS ====
 
-    def _planPathToCloseItemsPhase(self) -> tuple|None:
-        objects = self._current_state.get_closest_objects()
+    def _planPathToCloseItemsPhase(self) -> Action|None:
+        objects: list[dict]|None = self._current_state.get_room_objects(self._door['room_name'])
 
         if objects is None or len(objects) == 0:
+            # TODO Use correct format
             self._sendMessage("Can't find any objects in " + self._door['room_name'])
             self._phase = Phase.PLAN_PATH_TO_CLOSED_DOOR
         else:
-            print("Tracking objects...")
-            self._navigator.add_waypoints(map(lambda e: e['location'], objects))
+            print("Tracking objects...", objects)
+            collectable: list[dict] = list(filter(lambda e: 'CollectableBlock' in e['class_inheritance'], objects))
+            print("Found collectable objects:", collectable)
+
+            self._navigator.add_waypoints(map(lambda e: e['location'], collectable))
             self._phase = Phase.FOLLOW_PATH_TO_CLOSE_ITEMS
 
-    def _followPathToCloseItemsPhase(self) -> tuple | None:
+    def _followPathToCloseItemsPhase(self) -> Action|None:
         self._state_tracker.update(self._current_state)
 
         action = self._navigator.get_move_action(self._state_tracker)
